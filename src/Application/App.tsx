b00e2f5cd6e5e9ed74d258e0185298cf47026530
uVictorX <89784439+uVictorX@users.noreply.github.com>
2022-03-13 22:02:45 +0000

    Initial commit

---
+++ src/Application/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import AppRouter from '/src/Router/Routes'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <AppRouter />
+    </div>
+  ) 
+}
+
+export default App
---
+++ src/Router/Routes/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+
+import Home from '/src/Pages/Home'
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter
